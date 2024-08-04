f2b0d24164ec8a7108ad5f43aa9ea11487812788
Ayush5071 <ayusht5071@gmail.com>
2024-08-05 00:27:01 +0530

    generated frontend of Message Container and sideBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import Home from "./pages/Home/Home"
+// import { SignUp } from "./pages/signUp/SignUp"
+
+// import { Login } from "./pages/login/Login"
+
 function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      
+      <Home />
     </div>
   )
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,29 @@
-const login = () => {
+export const Login = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+        <div className="w-full p-6 rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+            <h1 className="text-3xl font-semibold text-center text-gray-300">
+                <span className="text-white">Chat App</span>
+            </h1>
+
+            <div>
+              <form action="">
+                <label className="label p-2">
+                    <span className="text-base label-text">Username</span>
+                </label>
+                <input type="text" placeholder="Enter Username" className="w-full input input-bordered h-10" />
+                <label className="label p-2">
+                    <span className="text-base label-text">Password</span>
+                </label>
+                <input type="password" placeholder="Enter Username" className="w-full input input-bordered h-10" />
+              </form>
+            </div>
+            <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+                {"Don't"} have an account?
+            </a>
+            <button className="btn btn-success btn-block mt-2">Login</button>
+        </div>
     </div>
   )
 }
 
-export default login
